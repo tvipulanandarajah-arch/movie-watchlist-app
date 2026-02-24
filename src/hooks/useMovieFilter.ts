4fefdc5a7c02912017ceb0a78377cf4c6c54d5f4
Thuvarakesh Vipulanandarajah <tvipulanandarajah@mitrai.com>
2026-02-24 07:09:51 +0530

    add useMovieFilter hook for filtering and sorting

---
+++ src/hooks/useMovieFilter.ts
@@ -0,0 +1,47 @@
+import { useState, useMemo } from "react";
+import type { Movie } from "../types/movie";
+import { type FilterType, FILTER_OPTIONS } from "../constants/appConfig";
+
+interface WatchlistCount {
+  all: number;
+  watched: number;
+  unwatched: number;
+}
+
+interface UseMovieFilterReturn {
+  filter: FilterType;
+  setFilter: (filter: FilterType) => void;
+  filteredMovies: Movie[];
+  watchlistCount: WatchlistCount;
+}
+
+export function useMovieFilter(movies: Movie[]): UseMovieFilterReturn {
+  const [filter, setFilter] = useState<FilterType>(FILTER_OPTIONS.ALL);
+
+  const filteredMovies = useMemo(() => {
+    switch (filter) {
+      case FILTER_OPTIONS.WATCHED:
+        return movies.filter((movie) => movie.watched);
+      case FILTER_OPTIONS.UNWATCHED:
+        return movies.filter((movie) => !movie.watched);
+      case FILTER_OPTIONS.ALL:
+      default:
+        return movies;
+    }
+  }, [movies, filter]);
+
+  const watchlistCount = useMemo((): WatchlistCount => {
+    return {
+      all: movies.length,
+      watched: movies.filter((movie) => movie.watched).length,
+      unwatched: movies.filter((movie) => !movie.watched).length,
+    };
+  }, [movies]);
+
+  return {
+    filter,
+    setFilter,
+    filteredMovies,
+    watchlistCount,
+  };
+}
